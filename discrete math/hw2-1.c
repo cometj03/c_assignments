cae5327bedab2ac20c6555d8026e121009db1433
정해성 <harryjung1001@gmail.com>
2022-05-19 13:40:59 +0900

    add comments

--- discrete math/hw2-1.c
+++ discrete math/hw2-1.c
@@ -8,16 +8,17 @@
 #include <stdio.h>
 #define SIZE 18
 
-/* 문자열을 사전순을 기준으로 비교하여
- * str1 > str2 일 때 1,
- * str1 == str2 일 때 0,
- * str1 < str2 일 때 -1을
- * 반환하는 함수 */
+/* 사전순을 기준으로 문자열을 비교하여
+* str1 > str2 일 때 1,
+* str1 = str2 일 때 0,
+* str1 < str2 일 때 -1을
+* 반환하는 함수 
+*/
 int compareString(char* a, char* b) {
 
 	while (*a != '\0' && *b != '\0' && *a == *b) a++, b++;
 
-	if (*a == *b) return 0;
+	if (*a == *b) return 0;	// *a와 *b가 같다는 의미는 둘 다 널 문자('\0')라는 뜻
 	if (*b == '\0' || *a > *b) return 1;
 	if (*a == '\0' || *a < *b) return -1;
 	return 0;
